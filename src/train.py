2c350f354257ada1d4bbb716bac8325d950233c9
adwaiyRanjith <adwaiy.ranjith@gmail.com>
2026-04-15 10:58:39 -0400

    added validation

--- src/train.py
+++ src/train.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 from model import model
 from config import LR, NUM_EPOCHS, NUM_CLASSES
-from dataset import train_loader
+from dataset import train_loader, val_loader
 
 if torch.cuda.is_available():
     device = "cuda"
@@ -23,7 +23,7 @@ optimizer = torch.optim.Adam(
 
 for epoch in range(NUM_EPOCHS):
     total_loss = 0
-
+    model.train()
     for images, labels in train_loader:
         images = images.to(device)
         labels = labels.to(device)
@@ -35,5 +35,15 @@ for epoch in range(NUM_EPOCHS):
         total_loss += loss.item()
 
     avg_loss = total_loss / len(train_loader)
-    print(f"Epoch {epoch+1}/{NUM_EPOCHS} | Avg Loss: {avg_loss:.4f}")
+    model.eval()
+    val_loss = 0;
+    with torch.no_grad():
+        for images, labels in val_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            val_loss += loss.item()
+    avg_val_loss = val_loss / len(val_loader)
+    print(f"Epoch {epoch+1}/{NUM_EPOCHS} | Train Loss: {avg_loss:.4f} | Val Loss: {avg_val_loss:.4f}")
 
